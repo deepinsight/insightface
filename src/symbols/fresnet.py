bcf375216cad268023be1c5cbdd24c15cbc15f2c
Jia Guo <guojia@gmail.com>
2017-12-03 22:23:28 +0800

    tiny

--- src/symbols/fresnet.py
+++ src/symbols/fresnet.py
@@ -131,6 +131,94 @@ def residual_unit_v1(data, num_filter, stride, dim_match, name, bottle_neck, **k
             shortcut._set_attr(mirror_stage='True')
         return Act(data=bn2 + shortcut, act_type='relu', name=name + '_relu3')
 
+def residual_unit_v1_L(data, num_filter, stride, dim_match, name, bottle_neck, **kwargs):
+    """Return ResNet Unit symbol for building ResNet
+    Parameters
+    ----------
+    data : str
+        Input data
+    num_filter : int
+        Number of output channels
+    bnf : int
+        Bottle neck channels factor with regard to num_filter
+    stride : tuple
+        Stride used in convolution
+    dim_match : Boolean
+        True means channel number between input and output is the same, otherwise means differ
+    name : str
+        Base name of the operators
+    workspace : int
+        Workspace used in convolution operator
+    """
+    use_se = kwargs.get('use_se', False)
+    bn_mom = kwargs.get('bn_mom', 0.9)
+    workspace = kwargs.get('workspace', 256)
+    memonger = kwargs.get('memonger', False)
+    #print('in unit1')
+    if bottle_neck:
+        conv1 = Conv(data=data, num_filter=int(num_filter*0.25), kernel=(1,1), stride=(1,1), pad=(0,0),
+                                   no_bias=True, workspace=workspace, name=name + '_conv1')
+        bn1 = mx.sym.BatchNorm(data=conv1, fix_gamma=False, eps=2e-5, momentum=bn_mom, name=name + '_bn1')
+        act1 = Act(data=bn1, act_type='relu', name=name + '_relu1')
+        conv2 = Conv(data=act1, num_filter=int(num_filter*0.25), kernel=(3,3), stride=(1,1), pad=(1,1),
+                                   no_bias=True, workspace=workspace, name=name + '_conv2')
+        bn2 = mx.sym.BatchNorm(data=conv2, fix_gamma=False, eps=2e-5, momentum=bn_mom, name=name + '_bn2')
+        act2 = Act(data=bn2, act_type='relu', name=name + '_relu2')
+        conv3 = Conv(data=act2, num_filter=num_filter, kernel=(1,1), stride=stride, pad=(0,0), no_bias=True,
+                                   workspace=workspace, name=name + '_conv3')
+        bn3 = mx.sym.BatchNorm(data=conv3, fix_gamma=False, eps=2e-5, momentum=bn_mom, name=name + '_bn3')
+
+        if use_se:
+          #se begin
+          body = mx.sym.Pooling(data=bn3, global_pool=True, kernel=(7, 7), pool_type='avg', name=name+'_se_pool1')
+          body = Conv(data=body, num_filter=num_filter//16, kernel=(1,1), stride=(1,1), pad=(0,0),
+                                    name=name+"_se_conv1", workspace=workspace)
+          body = Act(data=body, act_type='relu', name=name+'_se_relu1')
+          body = Conv(data=body, num_filter=num_filter, kernel=(1,1), stride=(1,1), pad=(0,0),
+                                    name=name+"_se_conv2", workspace=workspace)
+          body = mx.symbol.Activation(data=body, act_type='sigmoid', name=name+"_se_sigmoid")
+          bn3 = mx.symbol.broadcast_mul(bn3, body)
+          #se end
+
+        if dim_match:
+            shortcut = data
+        else:
+            conv1sc = Conv(data=data, num_filter=num_filter, kernel=(1,1), stride=stride, no_bias=True,
+                                            workspace=workspace, name=name+'_conv1sc')
+            shortcut = mx.sym.BatchNorm(data=conv1sc, fix_gamma=False, eps=2e-5, momentum=bn_mom, name=name + '_sc')
+        if memonger:
+            shortcut._set_attr(mirror_stage='True')
+        return Act(data=bn3 + shortcut, act_type='relu', name=name + '_relu3')
+    else:
+        conv1 = Conv(data=data, num_filter=num_filter, kernel=(3,3), stride=(1,1), pad=(1,1),
+                                      no_bias=True, workspace=workspace, name=name + '_conv1')
+        bn1 = mx.sym.BatchNorm(data=conv1, fix_gamma=False, momentum=bn_mom, eps=2e-5, name=name + '_bn1')
+        act1 = Act(data=bn1, act_type='relu', name=name + '_relu1')
+        conv2 = Conv(data=act1, num_filter=num_filter, kernel=(3,3), stride=stride, pad=(1,1),
+                                      no_bias=True, workspace=workspace, name=name + '_conv2')
+        bn2 = mx.sym.BatchNorm(data=conv2, fix_gamma=False, momentum=bn_mom, eps=2e-5, name=name + '_bn2')
+        if use_se:
+          #se begin
+          body = mx.sym.Pooling(data=bn2, global_pool=True, kernel=(7, 7), pool_type='avg', name=name+'_se_pool1')
+          body = Conv(data=body, num_filter=num_filter//16, kernel=(1,1), stride=(1,1), pad=(0,0),
+                                    name=name+"_se_conv1", workspace=workspace)
+          body = Act(data=body, act_type='relu', name=name+'_se_relu1')
+          body = Conv(data=body, num_filter=num_filter, kernel=(1,1), stride=(1,1), pad=(0,0),
+                                    name=name+"_se_conv2", workspace=workspace)
+          body = mx.symbol.Activation(data=body, act_type='sigmoid', name=name+"_se_sigmoid")
+          bn2 = mx.symbol.broadcast_mul(bn2, body)
+          #se end
+
+        if dim_match:
+            shortcut = data
+        else:
+            conv1sc = Conv(data=data, num_filter=num_filter, kernel=(1,1), stride=stride, no_bias=True,
+                                            workspace=workspace, name=name+'_conv1sc')
+            shortcut = mx.sym.BatchNorm(data=conv1sc, fix_gamma=False, momentum=bn_mom, eps=2e-5, name=name + '_sc')
+        if memonger:
+            shortcut._set_attr(mirror_stage='True')
+        return Act(data=bn2 + shortcut, act_type='relu', name=name + '_relu3')
+
 def residual_unit_v2(data, num_filter, stride, dim_match, name, bottle_neck, **kwargs):
     """Return ResNet Unit symbol for building ResNet
     Parameters
@@ -308,8 +396,12 @@ def residual_unit_v3(data, num_filter, stride, dim_match, name, bottle_neck, **k
 
 def residual_unit(data, num_filter, stride, dim_match, name, bottle_neck, **kwargs):
   uv = kwargs.get('version_unit', 1)
+  version_input = kwargs.get('version_input', 0)
   if uv==1:
-    return residual_unit_v1(data, num_filter, stride, dim_match, name, bottle_neck, **kwargs)
+    if version_input==0:
+      return residual_unit_v1(data, num_filter, stride, dim_match, name, bottle_neck, **kwargs)
+    else:
+      return residual_unit_v1_L(data, num_filter, stride, dim_match, name, bottle_neck, **kwargs)
   elif uv==2:
     return residual_unit_v2(data, num_filter, stride, dim_match, name, bottle_neck, **kwargs)
   else:
