99933878cd38d22f831aa2cf234b5c6ad4b8f28b
Jia Guo <guojia@gmail.com>
2017-12-03 11:37:47 +0800

    tiny

--- src/symbols/fresnet.py
+++ src/symbols/fresnet.py
@@ -376,6 +376,11 @@ def resnet(units, num_stages, filter_list, num_classes, bottle_neck, **kwargs):
       body = mx.symbol.Dropout(data=body, p=0.4)
       fc1 = mx.sym.FullyConnected(data=body, num_hidden=num_classes, name='pre_fc1')
       fc1 = mx.sym.BatchNorm(data=fc1, fix_gamma=True, eps=2e-5, momentum=bn_mom, name='fc1')
+    elif fc_type=='F':
+      body = mx.symbol.Dropout(data=body, p=0.4)
+      fc1 = mx.sym.FullyConnected(data=body, num_hidden=num_classes, name='pre_fc1')
+      fc1 = mx.sym.BatchNorm(data=fc1, fix_gamma=True, eps=2e-5, momentum=bn_mom, name='fc1')
+      fc1 = Act(data=fc1, act_type='relu', name='fc1_relu')
     else:
       bn1 = mx.sym.BatchNorm(data=body, fix_gamma=False, eps=2e-5, momentum=bn_mom, name='bn1')
       relu1 = Act(data=bn1, act_type='relu', name='relu1')
