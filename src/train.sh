1bd7db968cc867e538846aff1d31b6ee21cec337
Jia Guo <guojia@gmail.com>
2017-12-12 09:40:01 +0800

    tiny

--- src/train.sh
+++ src/train.sh
@@ -5,11 +5,11 @@ export MXNET_ENGINE_TYPE=ThreadedEnginePerDevice
 
 DATA_DIR=/opt/jiaguo/faces_vgg_112x112
 
-NETWORK=r49
+NETWORK=r50
 JOB=softmax1e3
 MODELDIR="../model-$NETWORK-$JOB"
 mkdir -p "$MODELDIR"
 PREFIX="$MODELDIR/model"
 LOGFILE="$MODELDIR/log"
-CUDA_VISIBLE_DEVICES='0,1,2,3' python -u train_softmax.py --data-dir $DATA_DIR --network "$NETWORK" --loss-type 0 --lr 0.1 --prefix "$PREFIX" --per-batch-size 128 --image-size '112,112' --version-input 1 --version-output E --version-unit 3 --use-se > "$LOGFILE" 2>&1 &
+CUDA_VISIBLE_DEVICES='0,1,2,3' python -u train_softmax.py --data-dir $DATA_DIR --network "$NETWORK" --loss-type 0 --prefix "$PREFIX" --per-batch-size 128 > "$LOGFILE" 2>&1 &
 
