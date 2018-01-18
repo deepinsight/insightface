a5a3ae30b3232251887ce7df129cbe64c6847acf
Jia Guo <guojia@gmail.com>
2018-01-18 17:18:17 +0800

    refine api

--- src/api/app.py
+++ src/api/app.py
@@ -26,6 +26,13 @@ app = Flask(__name__)
 def hello_world():
   return 'Hello, This is InsightFace!'
 
+def image_resize(image):
+  m = min(image.shape[0], image.shape[1])
+  f = 640.0/m
+  if f<1.0:
+    image = cv2.resize(image, (int(image.shape[1]*f), int(image.shape[0]*f)))
+  return image
+
 def get_image(data):
   image = None
   if 'url' in data:
@@ -37,6 +44,7 @@ def get_image(data):
     else:
       image = cv2.imread(url, cv2.IMREAD_COLOR)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = image_resize(image)
   elif 'data' in data:
     _bin = data['data']
     if _bin is not None:
@@ -45,6 +53,7 @@ def get_image(data):
         _bin = np.fromstring(_bin, np.uint8)
         image = cv2.imdecode(_bin, cv2.IMREAD_COLOR)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = image_resize(image)
       else:
         image = []
         for __bin in _bin:
@@ -52,6 +61,7 @@ def get_image(data):
           __bin = np.fromstring(__bin, np.uint8)
           _image = cv2.imdecode(__bin, cv2.IMREAD_COLOR)
           _image = cv2.cvtColor(_image, cv2.COLOR_BGR2RGB)
+          _image = image_resize(_image)
           image.append(_image)
 
   return image
@@ -63,19 +73,26 @@ def ver():
     values = json.loads(data)
     source_image = get_image(values['source'])
     if source_image is None:
+      print('source image is None')
       return '-1'
     assert not isinstance(source_image, list)
+    print(source_image.shape)
     target_image = get_image(values['target'])
     if target_image is None:
+      print('target image is None')
       return '-1'
+    #print(target_image.shape)
     if not isinstance(target_image, list):
       target_image = [target_image]
-    ret = model.is_same_id(source_image, target_image)
+    print('before call')
+    #ret = model.is_same_id(source_image, target_image)
+    ret = model.sim(source_image, target_image)
   except Exception as ex:
     print(ex)
     return '-1'
 
-  return str(int(ret))
+  #return str(int(ret))
+  return "%1.3f"%ret
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=18080, debug=True)
+    app.run('0.0.0.0', port=18080, debug=False)
