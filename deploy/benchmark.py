41d90393cbcd4157cdfa57ba7610c5466a91ed65
Jia Guo <guojia@gmail.com>
2018-01-26 23:18:53 +0800

    model slim

--- deploy/benchmark.py
+++ deploy/benchmark.py
@@ -7,7 +7,7 @@ import datetime
 parser = argparse.ArgumentParser(description='face model test')
 # general
 parser.add_argument('--image-size', default='112,112', help='')
-parser.add_argument('--model', default='../models/model-r34-amf/model,60', help='path to load model.')
+parser.add_argument('--model', default='../models/model-r34-amf/model,0', help='path to load model.')
 parser.add_argument('--gpu', default=0, type=int, help='gpu id')
 parser.add_argument('--det', default=2, type=int, help='mtcnn option, 2 means using R+O, else using O')
 parser.add_argument('--flip', default=0, type=int, help='whether do lr flip aug')
