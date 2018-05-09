a97ba7f947bd5b19c52efbaf79b22143c52e10a7
Jia Guo <guojia@gmail.com>
2018-05-09 16:18:38 +0800

    det option=0, support age, gender infer

--- deploy/test.py
+++ deploy/test.py
@@ -1,22 +1,32 @@
-import face_embedding
+import face_model
 import argparse
 import cv2
+import sys
 import numpy as np
 
 parser = argparse.ArgumentParser(description='face model test')
 # general
 parser.add_argument('--image-size', default='112,112', help='')
 parser.add_argument('--model', default='../models/model-r34-amf/model,0', help='path to load model.')
+parser.add_argument('--age-model', default='../models2/model-r34-age/model,0', help='path to load model.')
+parser.add_argument('--gender-model', default='../models2/model-r18-gender/model,0', help='path to load model.')
 parser.add_argument('--gpu', default=0, type=int, help='gpu id')
-parser.add_argument('--det', default=2, type=int, help='mtcnn option, 2 means using R+O, else using O')
+parser.add_argument('--det', default=0, type=int, help='mtcnn option, 1 means using R+O, 0 means detect from begining')
 parser.add_argument('--flip', default=0, type=int, help='whether do lr flip aug')
 parser.add_argument('--threshold', default=1.24, type=float, help='ver dist threshold')
 args = parser.parse_args()
 
-model = face_embedding.FaceModel(args)
+model = face_model.FaceModel(args)
 #img = cv2.imread('/raid5data/dplearn/lfw/Jude_Law/Jude_Law_0001.jpg')
 img = cv2.imread('/raid5data/dplearn/megaface/facescrubr/112x112/Tom_Hanks/Tom_Hanks_54745.png')
+img = model.get_input(img)
 f1 = model.get_feature(img)
+print(f1[0:10])
+age = model.get_age(img)
+print(age)
+gender = model.get_gender(img)
+print(gender)
+sys.exit(0)
 img = cv2.imread('/raid5data/dplearn/megaface/facescrubr/112x112/Tom_Hanks/Tom_Hanks_54733.png')
 f2 = model.get_feature(img)
 dist = np.sum(np.square(f1-f2))
