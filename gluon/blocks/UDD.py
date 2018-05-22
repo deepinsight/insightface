cec33b3362c73998f244f8466776cf0a1c93d934
nttstar <guojia@gmail.com>
2018-05-22 13:28:34 +0800

    fix gluon

--- gluon/blocks/UDD.py
+++ gluon/blocks/UDD.py
@@ -48,7 +48,7 @@ class EmbeddingBlock(gluon.HybridBlock):
         return x
         #return x
 
-class ArcMarginBlock(gluon.Block):
+class ArcMarginBlock(gluon.HybridBlock):
     def __init__(self, args, **kwargs):
       super(ArcMarginBlock, self).__init__(**kwargs)
       self.margin_s = args.margin_s
@@ -60,13 +60,11 @@ class ArcMarginBlock(gluon.Block):
       #self.weight = gluon.Parameter(name = 'fc7_weight', shape = (self.num_classes, self.emb_size))
       #self.weight.initialize()
       #self._weight = nd.empty(shape = (self.num_classes, self.emb_size))
-      if self.margin_a>0.0:
-        with self.name_scope():
-          self._weight = self.params.get('fc7_weight', shape=(self.num_classes, self.emb_size))
-          #if self.margin_a==0.0:
-          #  self._bias = self.params.get('fc7_bias', shape=(self.num_classes, 1), lr_mult=2.0, wd_mult=0.0)
-      else:
-        self.dense = nn.Dense(self.num_classes, prefix='fc7')
+      #if self.margin_a>0.0:
+      with self.name_scope():
+        self.fc7_weight = self.params.get('fc7_weight', shape=(self.num_classes, self.emb_size))
+      #else:
+      #  self.dense = nn.Dense(self.num_classes, prefix='fc7')
       self.body = nn.HybridSequential(prefix='')
       feat = fresnet.get(args.num_layers, 
           version_unit=args.version_unit,
@@ -78,51 +76,71 @@ class ArcMarginBlock(gluon.Block):
         feat = self.body(x)
         return feat
 
-    def forward(self, x, label):
+    def hybrid_forward(self, F, x, label, fc7_weight):
         feat = self.body(x)
         if self.margin_a==0.0:
-          fc7 = self.dense(feat)
+          fc7 = F.FullyConnected(feat, fc7_weight, no_bias = True, num_hidden=self.num_classes, name='fc7')
+          #fc7 = self.dense(feat)
           #with x.context:
           #  _w = self._weight.data()
             #_b = self._bias.data()
           #fc7 = nd.FullyConnected(data=feat, weight=_w, bias = _b, num_hidden=self.num_classes, name='fc7')
           #fc7 = F.softmax_cross_entropy(data = fc7, label=label)
-          return [fc7,label]
+          return fc7
 
-        with x.context:
-          _w = self._weight.data()
-
-        nx = nd.L2Normalization(feat, mode='instance', name='fc1n')*self.margin_s
-        w = nd.L2Normalization(_w, mode='instance')
-        fc7 = nd.FullyConnected(nx, w, no_bias = True, num_hidden=self.num_classes, name='fc7')
+        nx = F.L2Normalization(feat, mode='instance', name='fc1n')*self.margin_s
+        w = F.L2Normalization(fc7_weight, mode='instance')
+        fc7 = F.FullyConnected(nx, w, no_bias = True, num_hidden=self.num_classes, name='fc7')
         #fc7 = self.dense(nx)
         if self.margin_a!=1.0 or self.margin_m!=0.0 or self.margin_b!=0.0:
           if self.margin_a==1.0 and self.margin_m==0.0:
             s_m = s*self.margin_b
-            gt_one_hot = nd.one_hot(label, depth = self.num_classes, on_value = s_m, off_value = 0.0)
+            gt_one_hot = F.one_hot(label, depth = self.num_classes, on_value = s_m, off_value = 0.0)
             fc7 = fc7-gt_one_hot
           else:
-            zy = nd.pick(fc7, label, axis=1)
+            zy = F.pick(fc7, label, axis=1)
             cos_t = zy/self.margin_s
-            t = nd.arccos(cos_t)
+            t = F.arccos(cos_t)
             if self.margin_a!=1.0:
               t = t*self.margin_a
             if self.margin_m>0.0:
               t = t+self.margin_m
-            body = nd.cos(t)
+            body = F.cos(t)
             if self.margin_b>0.0:
               body = body - self.margin_b
             new_zy = body*self.margin_s
             diff = new_zy - zy
-            diff = nd.expand_dims(diff, 1)
-            gt_one_hot = nd.one_hot(label, depth = self.num_classes, on_value = 1.0, off_value = 0.0)
-            body = nd.broadcast_mul(gt_one_hot, diff)
+            diff = F.expand_dims(diff, 1)
+            gt_one_hot = F.one_hot(label, depth = self.num_classes, on_value = 1.0, off_value = 0.0)
+            body = F.broadcast_mul(gt_one_hot, diff)
             fc7 = fc7+body
-        return [fc7,label]
+        return fc7
 
     #def hybrid_forward(self, F, x):
     #  feat = self.body(x)
     #  return feat
+
+class DenseBlock(gluon.HybridBlock):
+    def __init__(self, args, **kwargs):
+      super(DenseBlock, self).__init__(**kwargs)
+      self.num_classes = args.num_classes
+      self.emb_size = args.emb_size
+      self.body = nn.HybridSequential(prefix='')
+      feat = fresnet.get(args.num_layers, 
+          version_unit=args.version_unit,
+          version_act=args.version_act)
+      self.body.add(feat)
+      self.body.add(EmbeddingBlock(args.emb_size, args.version_output, prefix=''))
+      self.dense = nn.Dense(self.num_classes, prefix='fc7')
+
+    def feature(self, x):
+        feat = self.body(x)
+        return feat
+
+    def hybrid_forward(self, F, x):
+        feat = self.body(x)
+        fc7 = self.dense(feat)
+        return fc7
 
 class ArcMarginTestBlock(gluon.Block):
     def __init__(self, args, **kwargs):
