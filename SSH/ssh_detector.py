0c651845fc6853d2213195638eccf87adc381610
Jia Guo <guojia@gmail.com>
2018-07-16 11:08:00 +0800

    ssh_detector fix

--- SSH/ssh_detector.py
+++ SSH/ssh_detector.py
@@ -13,7 +13,7 @@ from rcnn.processing.nms import gpu_nms_wrapper
 
 
 class SSHDetector:
-  def __init__(self, prefix, epoch, ctx_id=0):
+  def __init__(self, prefix, epoch, ctx_id=0, test_mode=False):
     self.ctx_id = ctx_id
     self.ctx = mx.gpu(self.ctx_id)
     self.fpn_keys = []
@@ -36,108 +36,110 @@ class SSHDetector:
     self.nms_threshold = 0.3
     self._bbox_pred = nonlinear_pred
     sym, arg_params, aux_params = mx.model.load_checkpoint(prefix, epoch)
-    #all_layers = sym.get_internals()
-    #outs = []
-    #for stride in self._feat_stride_fpn:
-    #  _name = 'rpn_cls_score_stride%s_output' % stride
-    #  rpn_cls_score = all_layers[_name]
-    #  rpn_cls_score_reshape = mx.symbol.Reshape(data=rpn_cls_score,
-    #                                            shape=(0, 2, -1, 0),
-    #                                            name="rpn_cls_score_reshape_stride%d" % stride)
-
-    #  rpn_cls_prob = mx.symbol.SoftmaxActivation(data=rpn_cls_score_reshape,
-    #                                             mode="channel",
-    #                                             name="rpn_cls_prob_stride%d" % stride)
-    #  rpn_cls_prob_reshape = mx.symbol.Reshape(data=rpn_cls_prob,
-    #                                           shape=(0, 2 * num_anchors, -1, 0),
-    #                                           name='rpn_cls_prob_reshape_stride%d' % stride)
-    #  outs.append(rpn_cls_prob_reshape)
-    #  _name = 'rpn_bbox_pred_stride%s_output' % stride
-    #  rpn_bbox_pred = all_layers[_name]
-    #  outs.append(rpn_bbox_pred)
-    #sym = mx.sym.Group(outs)
-
-    self.model = mx.mod.Module(symbol=sym, context=self.ctx, label_names = None)
-    image_size = (640, 640)
-    self.model.bind(data_shapes=[('data', (1, 3, image_size[0], image_size[1]))], for_training=False)
-    self.model.set_params(arg_params, aux_params)
     self.nms = gpu_nms_wrapper(self.nms_threshold, self.ctx_id)
-    pass
+    self.pixel_means = np.array([103.939, 116.779, 123.68]) #BGR
 
-  def detect(self,img, threshold=0.05):
-    image_size = (img.shape[0], img.shape[1])
-    #self.model.bind(data_shapes=[('data', (1, 3, image_size[0], image_size[1]))], for_training=False)
-    im_info = [image_size[0], image_size[1], 1.0]
-    data = nd.zeros( (1 ,3, image_size[0], image_size[1]) )
-    nimg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    nimg = np.transpose(nimg, (2,0,1))
-    nimg = nd.array(nimg)
-    data[0] = nimg
-    db = mx.io.DataBatch(data=(data,))
-    self.model.forward(db, is_train=False)
-    net_out = self.model.get_outputs()
+    if not test_mode:
+      image_size = (640, 640)
+      self.model = mx.mod.Module(symbol=sym, context=self.ctx, label_names = None)
+      self.model.bind(data_shapes=[('data', (1, 3, image_size[0], image_size[1]))], for_training=False)
+      self.model.set_params(arg_params, aux_params)
+    else:
+      from rcnn.core.module import MutableModule
+      image_size = (2400, 2400)
+      data_shape = [('data', (1,3,image_size[0], image_size[1]))]
+      self.model = MutableModule(symbol=sym, data_names=['data'], label_names=None,
+                                context=self.ctx, max_data_shapes=data_shape)
+      self.model.bind(data_shape, None, for_training=False)
+      self.model.set_params(arg_params, aux_params)
 
 
-    pre_nms_topN = self._rpn_pre_nms_top_n
-    #post_nms_topN = self._rpn_post_nms_top_n
-    #min_size_dict = self._rpn_min_size_fpn
-
+  def detect(self, img, threshold=0.05, scales=[1.0]):
     proposals_list = []
     scores_list = []
-    idx = 0
-    for s in self._feat_stride_fpn:
-        _key = 'stride%s'%s
-        stride = int(s)
-        scores = net_out[idx].asnumpy()
-        #print(scores.shape)
-        idx+=1
-        #print('scores',stride, scores.shape, file=sys.stderr)
-        scores = scores[:, self._num_anchors['stride%s'%s]:, :, :]
-        bbox_deltas = net_out[idx].asnumpy()
-        idx+=1
 
-        #if DEBUG:
-        #    print 'im_size: ({}, {})'.format(im_info[0], im_info[1])
-        #    print 'scale: {}'.format(im_info[2])
+    for im_scale in scales:
 
-        _height, _width = int(im_info[0] / stride), int(im_info[1] / stride)
-        height, width = bbox_deltas.shape[2], bbox_deltas.shape[3]
+      if im_scale!=1.0:
+        im = cv2.resize(img, None, None, fx=im_scale, fy=im_scale, interpolation=cv2.INTER_LINEAR)
+      else:
+        im = img
+      im = im.astype(np.float32)
+      #self.model.bind(data_shapes=[('data', (1, 3, image_size[0], image_size[1]))], for_training=False)
+      im_info = [im.shape[0], im.shape[1], im_scale]
+      im_tensor = np.zeros((1, 3, im.shape[0], im.shape[1]))
+      for i in range(3):
+          im_tensor[0, i, :, :] = im[:, :, 2 - i] - self.pixel_means[2 - i]
+      data = nd.array(im_tensor)
+      db = mx.io.DataBatch(data=(data,), provide_data=[('data', data.shape)])
+      self.model.forward(db, is_train=False)
+      net_out = self.model.get_outputs()
+      pre_nms_topN = self._rpn_pre_nms_top_n
+      #post_nms_topN = self._rpn_post_nms_top_n
+      #min_size_dict = self._rpn_min_size_fpn
 
-        A = self._num_anchors['stride%s'%s]
-        K = height * width
+      for s in self._feat_stride_fpn:
+          if len(scales)>1 and s==32 and im_scale==scales[-1]:
+            continue
+          _key = 'stride%s'%s
+          stride = int(s)
+          idx = 0
+          if s==16:
+            idx=2
+          elif s==8:
+            idx=4
+          print('getting', im_scale, stride, idx, len(net_out), data.shape, file=sys.stderr)
+          scores = net_out[idx].asnumpy()
+          #print(scores.shape)
+          idx+=1
+          #print('scores',stride, scores.shape, file=sys.stderr)
+          scores = scores[:, self._num_anchors['stride%s'%s]:, :, :]
+          bbox_deltas = net_out[idx].asnumpy()
 
-        anchors = anchors_plane(height, width, stride, self._anchors_fpn['stride%s'%s].astype(np.float32))
-        print((height, width), (_height, _width), anchors.shape, bbox_deltas.shape, scores.shape, file=sys.stderr)
-        anchors = anchors.reshape((K * A, 4))
+          #if DEBUG:
+          #    print 'im_size: ({}, {})'.format(im_info[0], im_info[1])
+          #    print 'scale: {}'.format(im_info[2])
 
-        #print('pre', bbox_deltas.shape, height, width)
-        bbox_deltas = self._clip_pad(bbox_deltas, (height, width))
-        #print('after', bbox_deltas.shape, height, width)
-        bbox_deltas = bbox_deltas.transpose((0, 2, 3, 1)).reshape((-1, 4))
+          _height, _width = int(im_info[0] / stride), int(im_info[1] / stride)
+          height, width = bbox_deltas.shape[2], bbox_deltas.shape[3]
 
-        scores = self._clip_pad(scores, (height, width))
-        scores = scores.transpose((0, 2, 3, 1)).reshape((-1, 1))
+          A = self._num_anchors['stride%s'%s]
+          K = height * width
 
-        #print(anchors.shape, bbox_deltas.shape, A, K, file=sys.stderr)
-        proposals = self._bbox_pred(anchors, bbox_deltas)
-        #proposals = anchors
+          anchors = anchors_plane(height, width, stride, self._anchors_fpn['stride%s'%s].astype(np.float32))
+          #print((height, width), (_height, _width), anchors.shape, bbox_deltas.shape, scores.shape, file=sys.stderr)
+          anchors = anchors.reshape((K * A, 4))
 
-        proposals = clip_boxes(proposals, im_info[:2])
+          #print('pre', bbox_deltas.shape, height, width)
+          bbox_deltas = self._clip_pad(bbox_deltas, (height, width))
+          #print('after', bbox_deltas.shape, height, width)
+          bbox_deltas = bbox_deltas.transpose((0, 2, 3, 1)).reshape((-1, 4))
 
-        #keep = self._filter_boxes(proposals, min_size_dict['stride%s'%s] * im_info[2])
-        #proposals = proposals[keep, :]
-        #scores = scores[keep]
-        #print('333', proposals.shape)
+          scores = self._clip_pad(scores, (height, width))
+          scores = scores.transpose((0, 2, 3, 1)).reshape((-1, 1))
 
-        scores_ravel = scores.ravel()
-        order = scores_ravel.argsort()[::-1]
-        if pre_nms_topN > 0:
-            order = order[:pre_nms_topN]
-        proposals = proposals[order, :]
-        scores = scores[order]
+          #print(anchors.shape, bbox_deltas.shape, A, K, file=sys.stderr)
+          proposals = self._bbox_pred(anchors, bbox_deltas)
+          #proposals = anchors
 
-        proposals_list.append(proposals)
-        scores_list.append(scores)
+          proposals = clip_boxes(proposals, im_info[:2])
+
+          #keep = self._filter_boxes(proposals, min_size_dict['stride%s'%s] * im_info[2])
+          #proposals = proposals[keep, :]
+          #scores = scores[keep]
+          #print('333', proposals.shape)
+
+          scores_ravel = scores.ravel()
+          order = scores_ravel.argsort()[::-1]
+          if pre_nms_topN > 0:
+              order = order[:pre_nms_topN]
+          proposals = proposals[order, :]
+          scores = scores[order]
+
+          proposals /= im_scale
+
+          proposals_list.append(proposals)
+          scores_list.append(scores)
 
     proposals = np.vstack(proposals_list)
     scores = np.vstack(scores_list)
@@ -191,4 +193,3 @@ class SSHDetector:
         tensor = tensor[:, :, :h, :w].copy()
 
       return tensor
-
