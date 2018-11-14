a042214852bbf061cbe898037ae88714e0735a1b
tengerye <yetengqi@gmail.com>
2018-11-14 09:53:54 +0800

    Compatiable with Python 3.

--- deploy/mtcnn_detector.py
+++ deploy/mtcnn_detector.py
@@ -6,7 +6,11 @@ import math
 import cv2
 from multiprocessing import Pool
 from itertools import repeat
-from itertools import izip
+try:
+    from itertools import izip
+except ImportError:
+    izip = zip
+
 from helper import nms, adjust_input, generate_bbox, detect_first_stage_warpper
 
 class MtcnnDetector(object):
