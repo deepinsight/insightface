d6176e4e620acc4de70ebcc50e6137c5a2932f77
lijc08 <lijc08@ubuntu-fast.com>
2018-12-03 11:49:59 +0800

    fix:dataset

--- src/train.sh
+++ src/train.sh
@@ -3,7 +3,7 @@ export MXNET_CPU_WORKER_NTHREADS=24
 export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
 export MXNET_ENGINE_TYPE=ThreadedEnginePerDevice
 
-DATA_DIR=/opt/jiaguo/faces_vgg_112x112
+DATA_DIR=/home/lijc08/datasets/glintasia/faces_glintasia
 
 NETWORK=r50
 JOB=softmax1e3
@@ -11,5 +11,6 @@ MODELDIR="../model-$NETWORK-$JOB"
 mkdir -p "$MODELDIR"
 PREFIX="$MODELDIR/model"
 LOGFILE="$MODELDIR/log"
-CUDA_VISIBLE_DEVICES='0,1,2,3' python -u train_softmax.py --data-dir $DATA_DIR --network "$NETWORK" --loss-type 0 --prefix "$PREFIX" --per-batch-size 128 > "$LOGFILE" 2>&1 &
+CUDA_VISIBLE_DEVICES='0,1,2,3' python -u train_softmax.py --data-dir $DATA_DIR --network "$NETWORK" --loss-type 0 --prefix "$PREFIX" --per-batch-size 128
+#> "$LOGFILE" 2>&1 &
 
