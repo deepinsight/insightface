5098b0b9f60ccd5a86c1f9596df798e8e0e0a580
lijc08 <lijc08@ubuntu-fast.com>
2019-01-18 19:26:13 +0800

    fxig

--- src/train.sh
+++ src/train.sh
@@ -7,7 +7,7 @@ DATA_DIR=~/datasets/glintasia
 PRETRAINED=~/deeplearning/insightface/model-r100-ii-1-15/model,5
 
 NETWORK=r100
-JOB=ii-1-16
+JOB=ii-1-18
 MODELDIR="../model-$NETWORK-$JOB"
 mkdir -p "$MODELDIR"
 PREFIX="$MODELDIR/model"
@@ -17,4 +17,4 @@ LOGFILE="$MODELDIR/log"
 
 #CUDA_VISIBLE_DEVICES='0' python -u train_softmax.py --lr-steps=10000,50000,80000,11000 --target=cfp_ff --verbose 2000 --data-dir $DATA_DIR --network "$NETWORK" --loss-type 5 --prefix "$PREFIX" --per-batch-size 64 > "$LOGFILE" 2>&1
 
-CUDA_VISIBLE_DEVICES='' python -u train_softmax.py --lr-steps=50000,100000,14000 --target=cfp_ff --verbose 2000 --data-dir $DATA_DIR --network "$NETWORK" --loss-type 5 --prefix "$PREFIX" --per-batch-size 512
+CUDA_VISIBLE_DEVICES='' python -u train_softmax.py --lr-steps=50000,100000,14000 --target=cfp_ff --verbose 2000 --data-dir $DATA_DIR --network "$NETWORK" --loss-type 5 --prefix "$PREFIX" --per-batch-size 32
