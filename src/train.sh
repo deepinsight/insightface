f13fd9a06ca18084baa38b6d2dab572c6170cadc
lijc08 <lijc08@ubuntu-fast.com>
2019-01-18 19:12:49 +0800

    fix

--- src/train.sh
+++ src/train.sh
@@ -17,4 +17,4 @@ LOGFILE="$MODELDIR/log"
 
 #CUDA_VISIBLE_DEVICES='0' python -u train_softmax.py --lr-steps=10000,50000,80000,11000 --target=cfp_ff --verbose 2000 --data-dir $DATA_DIR --network "$NETWORK" --loss-type 5 --prefix "$PREFIX" --per-batch-size 64 > "$LOGFILE" 2>&1
 
-python -u train_softmax.py --lr-steps=50000,100000,14000 --verbose 2000 --data-dir $DATA_DIR --network "$NETWORK" --loss-type 5 --prefix "$PREFIX" --per-batch-size 512 > "$LOGFILE" 2>&1
+python -u train_softmax.py --lr-steps=50000,100000,14000 --verbose 2000 --data-dir $DATA_DIR --network "$NETWORK" --loss-type 5 --prefix "$PREFIX" --per-batch-size 512
