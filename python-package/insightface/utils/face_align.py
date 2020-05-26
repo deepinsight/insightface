32ad1af3c7102d1254493c9f5b6d1409a16f627b
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-05-26 12:38:19 +0430

    fixed some errors and added comments.
    Right now norms can't be calculated due to src having extended dims, fixed the issue on that.
    Also added comment for left profile face, which was missing.

--- python-package/insightface/utils/face_align.py
+++ python-package/insightface/utils/face_align.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from skimage import transform as trans
 
+#<--left profile
 src1 = np.array([
      [51.642,50.115],
      [57.617,49.990],
@@ -68,6 +69,7 @@ def estimate_norm(lmk, image_size = 112, mode='arcface'):
     src = arcface_src
   else:
     src = src_map[image_size]
+    src.reshape(-1, 5, 2)
   for i in np.arange(src.shape[0]):
     tform.estimate(lmk, src[i])
     M = tform.params[0:2,:]
