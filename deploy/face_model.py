fd10b74b45692e53047149cb4d526067845b4a7d
nttstar <guojia@gmail.com>
2020-11-07 13:22:05 +0800

    use retinaface in deploy/face_model

--- deploy/face_model.py
+++ deploy/face_model.py
@@ -2,23 +2,14 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from scipy import misc
 import sys
 import os
 import argparse
-#import tensorflow as tf
 import numpy as np
 import mxnet as mx
-import random
 import cv2
-import sklearn
-from sklearn.decomposition import PCA
-from time import sleep
-from easydict import EasyDict as edict
-from mtcnn_detector import MtcnnDetector
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src', 'common'))
-import face_image
-import face_preprocess
+import insightface
+from insightface.utils import face_align
 
 
 def do_flip(data):
@@ -26,11 +17,7 @@ def do_flip(data):
         data[idx, :, :] = np.fliplr(data[idx, :, :])
 
 
-def get_model(ctx, image_size, model_str, layer):
-    _vec = model_str.split(',')
-    assert len(_vec) == 2
-    prefix = _vec[0]
-    epoch = int(_vec[1])
+def get_model(ctx, image_size, prefix, epoch, layer):
     print('loading', prefix, epoch)
     sym, arg_params, aux_params = mx.model.load_checkpoint(prefix, epoch)
     all_layers = sym.get_internals()
@@ -43,77 +30,38 @@ def get_model(ctx, image_size, model_str, layer):
 
 
 class FaceModel:
-    def __init__(self, args):
-        self.args = args
-        ctx = mx.gpu(args.gpu)
-        _vec = args.image_size.split(',')
-        assert len(_vec) == 2
-        image_size = (int(_vec[0]), int(_vec[1]))
-        self.model = None
-        self.ga_model = None
-        if len(args.model) > 0:
-            self.model = get_model(ctx, image_size, args.model, 'fc1')
-        if len(args.ga_model) > 0:
-            self.ga_model = get_model(ctx, image_size, args.ga_model, 'fc1')
-
-        self.threshold = args.threshold
-        self.det_minsize = 50
-        self.det_threshold = [0.6, 0.7, 0.8]
-        #self.det_factor = 0.9
-        self.image_size = image_size
-        mtcnn_path = os.path.join(os.path.dirname(__file__), 'mtcnn-model')
-        if args.det == 0:
-            detector = MtcnnDetector(model_folder=mtcnn_path,
-                                     ctx=ctx,
-                                     num_worker=1,
-                                     accurate_landmark=True,
-                                     threshold=self.det_threshold)
+    def __init__(self, ctx_id, model_prefix, model_epoch, use_large_detector=False):
+        if use_large_detector:
+            self.detector = insightface.model_zoo.get_model('retinaface_r50_v1')
         else:
-            detector = MtcnnDetector(model_folder=mtcnn_path,
-                                     ctx=ctx,
-                                     num_worker=1,
-                                     accurate_landmark=True,
-                                     threshold=[0.0, 0.0, 0.2])
-        self.detector = detector
+            self.detector = insightface.model_zoo.get_model('retinaface_mnet025_v2')
+        self.detector.prepare(ctx_id=ctx_id)
+        if ctx_id>=0:
+            ctx = mx.gpu(ctx_id)
+        else:
+            ctx = mx.cpu()
+        image_size = (112,112)
+        self.model = get_model(ctx, image_size, model_prefix, model_epoch, 'fc1')
+        self.image_size = image_size
 
     def get_input(self, face_img):
-        ret = self.detector.detect_face(face_img, det_type=self.args.det)
-        if ret is None:
-            return None
-        bbox, points = ret
-        if bbox.shape[0] == 0:
+        bbox, pts5 = self.detector.detect(face_img, threshold=0.8)
+        if bbox.shape[0]==0:
             return None
         bbox = bbox[0, 0:4]
-        points = points[0, :].reshape((2, 5)).T
-        #print(bbox)
-        #print(points)
-        nimg = face_preprocess.preprocess(face_img,
-                                          bbox,
-                                          points,
-                                          image_size='112,112')
-        nimg = cv2.cvtColor(nimg, cv2.COLOR_BGR2RGB)
-        aligned = np.transpose(nimg, (2, 0, 1))
-        return aligned
+        pts5 = pts5[0, :]
+        nimg = face_align.norm_crop(face_img, pts5)
+        return nimg
 
     def get_feature(self, aligned):
-        input_blob = np.expand_dims(aligned, axis=0)
+        a = cv2.cvtColor(aligned, cv2.COLOR_BGR2RGB)
+        a = np.transpose(a, (2, 0, 1))
+        input_blob = np.expand_dims(a, axis=0)
         data = mx.nd.array(input_blob)
         db = mx.io.DataBatch(data=(data, ))
         self.model.forward(db, is_train=False)
-        embedding = self.model.get_outputs()[0].asnumpy()
-        embedding = sklearn.preprocessing.normalize(embedding).flatten()
-        return embedding
+        emb = self.model.get_outputs()[0].asnumpy()[0]
+        norm = np.sqrt(np.sum(emb*emb)+0.00001)
+        emb /= norm
+        return emb
 
-    def get_ga(self, aligned):
-        input_blob = np.expand_dims(aligned, axis=0)
-        data = mx.nd.array(input_blob)
-        db = mx.io.DataBatch(data=(data, ))
-        self.ga_model.forward(db, is_train=False)
-        ret = self.ga_model.get_outputs()[0].asnumpy()
-        g = ret[:, 0:2].flatten()
-        gender = np.argmax(g)
-        a = ret[:, 2:202].reshape((100, 2))
-        a = np.argmax(a, axis=1)
-        age = int(sum(a))
-
-        return gender, age
