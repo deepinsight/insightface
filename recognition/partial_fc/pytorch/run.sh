289a6f0925aa374c7f0bd406b63d9d90ededb27f
xiaoyang-coder <43001198+xiaoyang-coder@users.noreply.github.com>
2020-11-13 23:13:48 +0800

    Update run.sh

--- recognition/partial_fc/pytorch/run.sh
+++ recognition/partial_fc/pytorch/run.sh
@@ -4,7 +4,7 @@ starttime=`date +'%Y-%m-%d %H:%M:%S'`
 
 
 # /usr/bin/
-python3 -m torch.distributed.launch --nproc_per_node=8 --nnodes=1 --node_rank=0 --master_addr="127.0.0.1" --master_port=1234 partial_fc.py
+python3 -m torch.distributed.launch --nproc_per_node=8 --nnodes=1 --node_rank=0 --master_addr="127.0.0.1" --master_port=1234 partial_fc.py  | tee hist.log
 
 
 endtime=`date +'%Y-%m-%d %H:%M:%S'`
