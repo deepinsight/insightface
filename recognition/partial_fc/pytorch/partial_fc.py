3137db737d0025d80f4da3edf54ec440f58c3b5c
leiyu1980 <cdzhanglei@gmail.com>
2021-01-07 12:43:35 +0800

    Update partial_fc.py
    fp16 Checkpoint

--- recognition/partial_fc/pytorch/partial_fc.py
+++ recognition/partial_fc/pytorch/partial_fc.py
@@ -7,14 +7,16 @@ https://arxiv.org/abs/2010.05222
 
 import argparse
 import time
-
+import os
+import os.path
 import torch
 import torch.distributed as dist
 import torch.nn.functional as F
 import torch.utils.data.distributed
 from torch import nn
 from torch.utils.tensorboard import SummaryWriter
-
+import torch.cuda.amp
+from torch.cuda.amp import autocast,GradScaler
 import backbones
 from config import config as cfg
 from dataset import MXFaceDataset, DataLoaderX
@@ -67,9 +69,10 @@ def main(local_rank):
     torch.cuda.set_device(local_rank)
     cfg.rank = dist.get_rank()
     cfg.world_size = dist.get_world_size()
+    print("world_size:", cfg.world_size)
     trainset = MXFaceDataset(root_dir=cfg.rec, local_rank=local_rank)
     train_sampler = torch.utils.data.distributed.DistributedSampler(
-        trainset, shuffle=True)
+        trainset, shuffle=True) 
     train_loader = DataLoaderX(local_rank=local_rank,
                                dataset=trainset,
                                batch_size=cfg.batch_size,
@@ -77,45 +80,56 @@ def main(local_rank):
                                num_workers=0,
                                pin_memory=True,
                                drop_last=False)
-
-    backbone = backbones.iresnet100(False).to(local_rank)
+ 
+    backbone = backbones.iefficientnet(False,num_features=cfg.embedding_size)
     backbone.train()
+        # Broadcast init parameters
 
-    # Broadcast init parameters
+    if cfg.preload:
+        print("Loading Backbone Checkpoint '{}'".format(cfg.preload))
+        loc = 'cuda:{}'.format(cfg.local_rank)
+        backbone.load_state_dict(torch.load(cfg.preload, map_location=loc))
+    backbone=backbone.to(local_rank)
+
+    backbone.train()
     for ps in backbone.parameters():
-        dist.broadcast(ps, 0)
+      dist.broadcast(ps, 0)
 
     # DDP
     backbone = torch.nn.parallel.DistributedDataParallel(
-        module=backbone,
-        broadcast_buffers=False,
-        device_ids=[cfg.local_rank])
+        module=backbone, broadcast_buffers=False, device_ids=[cfg.local_rank])
     backbone.train()
-
     # Memory classifer
-    dist_sample_classifer = DistSampleClassifier(
-        rank=dist.get_rank(),
-        local_rank=local_rank,
-        world_size=cfg.world_size)
+    dist_sample_classifer = DistSampleClassifier(rank=dist.get_rank(),
+                                                 local_rank=local_rank,
+                                                 world_size=cfg.world_size)
+    if cfg.pf_preload:
+        file_model = os.path.join(cfg.pf_preload,str(cfg.local_rank)+'_pf.pth')
+        print("Loading pf Checkpoint '{}'".format(file_model))
+        loc = 'cuda:{}'.format(cfg.local_rank)        
+        dist_sample_classifer.load_state_dict(torch.load(file_model, map_location=loc))
+    dist_sample_classifer=dist_sample_classifer.to(local_rank)
+                                    
 
     # Margin softmax
-    margin_softmax = MarginSoftmax(s=64.0, m=0.4)
+    margin_softmax = MarginSoftmax(s=64.0, m=0.2)
 
     # Optimizer for backbone and classifer
     optimizer = SGD([{
         'params': backbone.parameters()
     }, {
-        'params': dist_sample_classifer.parameters()
+        'params': dist_sample_classifer.parameters(),'lr': cfg.lr
+        # 'params': dist_sample_classifer.parameters()
     }],
         lr=cfg.lr,
-        momentum=0.9,
+        momentum=cfg.momentum,
         weight_decay=cfg.weight_decay,
         rescale=cfg.world_size)
 
     # Lr scheduler
     scheduler = torch.optim.lr_scheduler.LambdaLR(
-        optimizer=optimizer,
-        lr_lambda=cfg.lr_func)
+                                                  optimizer=optimizer,
+                                                  lr_lambda=cfg.lr_func)
     n_epochs = cfg.num_epoch
     start_epoch = 0
 
@@ -130,56 +144,111 @@ def main(local_rank):
     losses = AverageMeter()
     global_step = 0
     train_start = time.time()
+    if cfg.fp16:
+        scaler = GradScaler()
+        
     for epoch in range(start_epoch, n_epochs):
         train_sampler.set_epoch(epoch)
         for step, (img, label) in enumerate(train_loader):
-            total_label, norm_weight = dist_sample_classifer.prepare(
+            # total_label, norm_weight = dist_sample_classifer.prepare(
+            #     label, optimizer)
+            if cfg.fp16:
+                with autocast():
+                    total_label, norm_weight = dist_sample_classifer.prepare(
                 label, optimizer)
-            features = F.normalize(backbone(img))
+                    features = F.normalize(backbone(img))
 
-            # Features all-gather
-            total_features = torch.zeros(features.size()[0] * cfg.world_size,
-                                         cfg.embedding_size,
-                                         device=local_rank)
-            dist.all_gather(list(total_features.chunk(cfg.world_size, dim=0)),
-                            features.data)
-            total_features.requires_grad = True
+                    # Features all-gather
+                    total_features = torch.zeros(features.size()[0] * cfg.world_size,
+                                                cfg.embedding_size,
+                                                device=local_rank)
+                    dist.all_gather(list(total_features.chunk(cfg.world_size, dim=0)),
+                                    features.data)
+                    total_features.requires_grad = True
 
-            # Calculate logits
-            logits = dist_sample_classifer(total_features, norm_weight)
-            logits = margin_softmax(logits, total_label)
+                    # Calculate logits
+                    logits = dist_sample_classifer(total_features, norm_weight)
+                    logits = margin_softmax(logits, total_label)
 
-            with torch.no_grad():
-                max_fc = torch.max(logits, dim=1, keepdim=True)[0]
-                dist.all_reduce(max_fc, dist.ReduceOp.MAX)
+                    with torch.no_grad():
+                        max_fc = torch.max(logits, dim=1, keepdim=True)[0]
+                        dist.all_reduce(max_fc, dist.ReduceOp.MAX)
 
-                # Calculate exp(logits) and all-reduce
-                logits_exp = torch.exp(logits - max_fc)
-                logits_sum_exp = logits_exp.sum(dim=1, keepdims=True)
-                dist.all_reduce(logits_sum_exp, dist.ReduceOp.SUM)
+                        # Calculate exp(logits) and all-reduce
+                        logits_exp = torch.exp(logits - max_fc)
+                        logits_sum_exp = logits_exp.sum(dim=1, keepdims=True)
+                        dist.all_reduce(logits_sum_exp, dist.ReduceOp.SUM)
 
-                # Calculate prob
-                logits_exp.div_(logits_sum_exp)
+                        # Calculate prob
+                        logits_exp.div_(logits_sum_exp)
 
-                # Get one-hot
-                grad = logits_exp
-                index = torch.where(total_label != -1)[0]
-                one_hot = torch.zeros(index.size()[0],
-                                      grad.size()[1],
-                                      device=grad.device)
-                one_hot.scatter_(1, total_label[index, None], 1)
+                        # Get one-hot
+                        grad = logits_exp
+                        index = torch.where(total_label != -1)[0]
+                        one_hot = torch.zeros(index.size()[0],
+                                            grad.size()[1],
+                                            device=grad.device)
+                        one_hot.scatter_(1, total_label[index, None], 1)
 
-                # Calculate loss
-                loss = torch.zeros(grad.size()[0], 1, device=grad.device)
-                loss[index] = grad[index].gather(1, total_label[index, None])
-                dist.all_reduce(loss, dist.ReduceOp.SUM)
-                loss_v = loss.clamp_min_(1e-30).log_().mean() * (-1)
+                        # Calculate loss
+                        loss = torch.zeros(grad.size()[0], 1, device=grad.device)
+                        loss[index] = grad[index].gather(1, total_label[index, None])
+                        dist.all_reduce(loss, dist.ReduceOp.SUM)
+                        loss_v = loss.clamp_min_(1e-30).log_().mean() * (-1)
 
-                # Calculate grad
-                grad[index] -= one_hot
-                grad.div_(features.size()[0])
+                        # Calculate grad
+                        grad[index] -= one_hot
+                        grad.div_(features.size()[0])
+            else:
+                total_label, norm_weight = dist_sample_classifer.prepare(
+                label, optimizer)
+                features = F.normalize(backbone(img))
+                    # Features all-gather
+                total_features = torch.zeros(features.size()[0] * cfg.world_size,
+                                            cfg.embedding_size,
+                                            device=local_rank)
+                dist.all_gather(list(total_features.chunk(cfg.world_size, dim=0)),
+                                features.data)
+                total_features.requires_grad = True
 
-            logits.backward(grad)
+                # Calculate logits
+                logits = dist_sample_classifer(total_features, norm_weight)
+                logits = margin_softmax(logits, total_label)
+
+                with torch.no_grad():
+                    max_fc = torch.max(logits, dim=1, keepdim=True)[0]
+                    dist.all_reduce(max_fc, dist.ReduceOp.MAX)
+
+                    # Calculate exp(logits) and all-reduce
+                    logits_exp = torch.exp(logits - max_fc)
+                    logits_sum_exp = logits_exp.sum(dim=1, keepdims=True)
+                    dist.all_reduce(logits_sum_exp, dist.ReduceOp.SUM)
+
+                    # Calculate prob
+                    logits_exp.div_(logits_sum_exp)
+
+                    # Get one-hot
+                    grad = logits_exp
+                    index = torch.where(total_label != -1)[0]
+                    one_hot = torch.zeros(index.size()[0],
+                                        grad.size()[1],
+                                        device=grad.device)
+                    one_hot.scatter_(1, total_label[index, None], 1)
+
+                    # Calculate loss
+                    loss = torch.zeros(grad.size()[0], 1, device=grad.device)
+                    loss[index] = grad[index].gather(1, total_label[index, None])
+                    dist.all_reduce(loss, dist.ReduceOp.SUM)
+                    loss_v = loss.clamp_min_(1e-30).log_().mean() * (-1)
+
+                    # Calculate grad
+                    grad[index] -= one_hot
+                    grad.div_(features.size()[0])
+
+            if cfg.fp16:
+                scaler.scale(logits).backward(grad)
+            else:
+                logits.backward(grad)
             if total_features.grad is not None:
                 total_features.grad.detach_()
             x_grad = torch.zeros_like(features)
@@ -189,36 +258,55 @@ def main(local_rank):
                 x_grad, list(total_features.grad.chunk(cfg.world_size, dim=0)))
             x_grad.mul_(cfg.world_size)
             # Backward backbone
-            features.backward(x_grad)
-            optimizer.step()
+            if cfg.fp16:
+                scaler.scale(features).backward(x_grad)
+                scaler.step(optimizer)
+                
+            else:
+                features.backward(x_grad)
+                optimizer.step()
 
             # Update classifer
             dist_sample_classifer.update()
             optimizer.zero_grad()
+            # losses.update(loss_v, 1)
+            # if cfg.fp16:
+            #     scaler.update()
+
             losses.update(loss_v, 1)
             if cfg.local_rank == 0 and step % 50 == 0:
                 time_now = (time.time() - train_start) / 3600
                 time_total = time_now / ((global_step + 1) / total_step)
-                time_for_end = time_total - time_now
+                time_for_end = time_total - time_now 
                 writer.add_scalar('time_for_end', time_for_end, global_step)
                 writer.add_scalar('loss', loss_v, global_step)
-                print("Speed %d samples/sec   Loss %.4f   Epoch: %d   Global Step: %d   Required: %1.f hours" %
+                writer.add_scalar('loss_epoch', loss_v,scheduler.get_lr()[0])
+                # writer.add_scalar('loss_lr', loss_v, scheduler.get_lr()[0])
+                print("Speed %d samples/sec   Loss %.4f   Epoch: %d   Global Step: %d lr: %0.6f   Required: %1.f hours" %
                       (
                           (cfg.batch_size * global_step / (time.time() - train_start) * cfg.world_size),
                           losses.avg,
                           epoch,
                           global_step,
+                          scheduler.get_lr()[0],
                           time_for_end
                       ))
                 losses.reset()
+            if cfg.fp16:
+                scaler.update()
 
             global_step += 1
-        scheduler.step()
-        if dist.get_rank() == 0:
-            import os
+        scheduler.step()      
+        if dist.get_rank() == 0:            
             if not os.path.exists(cfg.output):
                 os.makedirs(cfg.output)
             torch.save(backbone.module.state_dict(), os.path.join(cfg.output, str(epoch) + 'backbone.pth'))
+        #save pf        
+        for gpu_index in range(cfg.world_size):
+            if not os.path.exists(cfg.output):
+                os.makedirs(cfg.output)
+            torch.save(dist_sample_classifer.state_dict(), os.path.join(cfg.output, str(dist.get_rank()) + '_pf.pth'))
+        
     dist.destroy_process_group()
 
 
