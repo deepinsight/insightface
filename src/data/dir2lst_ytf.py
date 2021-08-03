bf8b58f9ac32be44089bf45956534a8651721afe
eeric <eeric@users.noreply.github.com>
2021-08-03 16:26:34 +0800

    Update dir2lst_ytf.py

--- src/data/dir2lst_ytf.py
+++ src/data/dir2lst_ytf.py
@@ -1,8 +1,15 @@
+# -*- coding: utf-8 -*-
+#aligned_images_DB 
+#--john
+#----12.jpg
+#----13.jpg
+
 import sys
 import os
 from easydict import EasyDict as edict
 
 input_dir = '/raid5data/dplearn/YTF/aligned_images_DB'
+fp = open('prefix.lst', 'w')
 ret = []
 label = 0
 person_names = []
@@ -13,20 +20,21 @@ for person_name in person_names:
   _subdir = os.path.join(input_dir, person_name)
   if not os.path.isdir(_subdir):
     continue
-  for _subdir2 in os.listdir(_subdir):
-    _subdir2 = os.path.join(_subdir, _subdir2)
-    if not os.path.isdir(_subdir2):
-      continue
-    _ret = []
-    for img in os.listdir(_subdir2):
+  #for _subdir2 in os.listdir(_subdir):
+    #_subdir2 = os.path.join(_subdir, _subdir2)
+    #if not os.path.isdir(_subdir2):
+    #  continue
+  _ret = []
+  for img in os.listdir(_subdir):
       fimage = edict()
-      fimage.id = os.path.join(_subdir2, img)
+      fimage.id = os.path.join(_subdir, img)
       fimage.classname = str(label)
-      fimage.image_path = os.path.join(_subdir2, img)
+      fimage.image_path = os.path.join(_subdir, img)
       fimage.bbox = None
       fimage.landmark = None
       _ret.append(fimage)
-    ret += _ret
+  ret += _ret
   label+=1
 for item in ret:
   print("%d\t%s\t%d" % (1, item.image_path, int(item.classname)))
+  fp.write("%d\t%s\t%d\n" % (1, item.image_path, int(item.classname)))
