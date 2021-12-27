05c600d0acdc216ea1a76bf0c87307cb4074b8a0
romakail <roman.kail@mail.ru>
2021-12-28 00:58:16 +0700

    Changed config import method

--- recognition/arcface_torch/train.py
+++ recognition/arcface_torch/train.py
@@ -29,7 +29,8 @@ def main(args):
         rank = 0
         dist.init_process_group(backend='nccl', init_method="tcp://127.0.0.1:12584", rank=rank, world_size=world_size)
 
-    local_rank = args.local_rank
+    # local_rank = args.local_rank
+    local_rank = int(os.environ['LOCAL_RANK'])
     torch.cuda.set_device(local_rank)
     os.makedirs(cfg.output, exist_ok=True)
     init_logging(rank, cfg.output)
--- recognition/arcface_torch/utils/utils_config.py
+++ recognition/arcface_torch/utils/utils_config.py
@@ -4,11 +4,15 @@ import os.path as osp
 
 def get_config(config_file):
     assert config_file.startswith('configs/'), 'config file setting must start with configs/'
-    temp_config_name = osp.basename(config_file)
-    temp_module_name = osp.splitext(temp_config_name)[0]
+    # temp_config_name = osp.basename(config_file)
+    # temp_module_name = osp.splitext(temp_config_name)[0]
+
+    temp_module_name = osp.splitext(config_file)[0]
+    temp_module_name = temp_module_name.replace("/", ".")
     config = importlib.import_module("configs.base")
     cfg = config.config
-    config = importlib.import_module("configs.scale.%s" % temp_module_name)
+    # config = importlib.import_module("configs.scale.%s" % temp_module_name)
+    config = importlib.import_module(temp_module_name)
     job_cfg = config.config
     cfg.update(job_cfg)
     if cfg.output is None:
