6589747914e0fbf58f6065aabc1f0a700cfb34b8
AnXiang <anxiangsir@outlook.com>
2022-01-17 14:13:00 +0800

    added configs

--- recognition/arcface_torch/configs/glint360k_r100_lr02_bs4k_16gpus.py
+++ recognition/arcface_torch/configs/glint360k_r100_lr02_bs4k_16gpus.py
@@ -5,7 +5,7 @@ from easydict import EasyDict as edict
 # mount -t tmpfs -o size=140G  tmpfs /train_tmp
 
 config = edict()
-config.loss = "arcface"
+config.loss = "cosface"
 config.network = "r100"
 config.resume = False
 config.output = None
@@ -14,14 +14,14 @@ config.sample_rate = 1.0
 config.fp16 = True
 config.momentum = 0.9
 config.weight_decay = 5e-4
-config.batch_size = 128
-config.lr = 0.2
-config.verbose = 2000
+config.batch_size = 256
+config.lr = 0.4
+config.verbose = 5000
 config.dali = False
 
 config.rec = "/train_tmp/glint360k"
 config.num_classes = 360232
 config.num_image = 17091657
 config.num_epoch = 20
-config.warmup_epoch = 0
+config.warmup_epoch = 2
 config.val_targets = ['lfw', 'cfp_fp', "agedb_30"]
