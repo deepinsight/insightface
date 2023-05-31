aaac82ab9ce654de60885b7b878f2c3154230fd2
lorenzo.mirante <lorenzo.mirante@smarteye.se>
2023-05-31 22:33:27 +0200

    Cleanup-and-improve-the-examples

--- examples/in_swapper/inswapper_main.py
+++ examples/in_swapper/inswapper_main.py
@@ -1,26 +1,23 @@
-import datetime
-import numpy as np
-import os
-import os.path as osp
-import glob
 import cv2
-import insightface
+import numpy as np
 from insightface.app import FaceAnalysis
 from insightface.data import get_image as ins_get_image
 
+import insightface
 
-assert insightface.__version__>='0.7'
+assert insightface.__version__ >= "0.7"
 
-if __name__ == '__main__':
-    app = FaceAnalysis(name='buffalo_l')
+if __name__ == "__main__":
+    app = FaceAnalysis(name="buffalo_l")
     app.prepare(ctx_id=0, det_size=(640, 640))
-    swapper = insightface.model_zoo.get_model('inswapper_128.onnx', download=True, download_zip=True)
+    swapper = insightface.model_zoo.get_model(
+        "inswapper_128.onnx", download=True, download_zip=True
+    )
 
-
-    img = ins_get_image('t1')
+    img = ins_get_image("t1")
     faces = app.get(img)
-    faces = sorted(faces, key = lambda x : x.bbox[0])
-    assert len(faces)==6
+    faces = sorted(faces, key=lambda x: x.bbox[0])
+    assert len(faces) == 6
     source_face = faces[2]
     res = img.copy()
     for face in faces:
@@ -32,5 +29,3 @@ if __name__ == '__main__':
         res.append(_img)
     res = np.concatenate(res, axis=1)
     cv2.imwrite("./t1_swapped2.jpg", res)
-
-
